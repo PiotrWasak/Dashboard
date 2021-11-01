490cc3ffee0fd7431a7e797174834375b5fba269
PiotrWasak <68294222+PiotrWasak@users.noreply.github.com>
2021-11-01 12:07:27 +0100

    Working

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
     <NavBar />
-    <SideBar />
+    <SideBar @change-dash-view="changeDashView" />
 
     <dashboard-main>
       <div class="container-fluid">
-        <Calc />
+        <div v-if="currentView==='Dashboard'">Dashboard Main</div>
+        <todo v-if='currentView==="Todo"' />
+        <calc v-if='currentView==="Calc"'/>
+        <currency-converter v-if='currentView==="CurrencyConverter"'/>
       </div>
     </dashboard-main>
   </div>
@@ -17,6 +20,7 @@ import SideBar from "./components/SideBar.vue";
 import NavBar from "./components/NavBar.vue";
 import Calc from "calc/src/App.vue";
 import Todo from "todo/src/App.vue";
+import CurrencyConverter from "currencyconverter/src/App.vue"
 import DashboardMain from "./components/DashboardMain.vue";
 
 export default {
@@ -27,16 +31,23 @@ export default {
     Todo,
     Calc,
     DashboardMain,
+    CurrencyConverter
   },
   data() {
     return {
-      dashViews: ["Calc", "CurrencyConverter", "TODO", "TicTacToe"],
+      dashViews: ["Calc", "CurrencyConverter", "Todo"],
+      currentView: 'Todo',
     };
   },
+  methods: {
+    changeDashView: function(viewName) {
+      this.currentView = viewName;
+    }
+  }
 };
 </script>
 
-<style>
+<style scoped>
 body {
   font-size: 0.875rem;
 }
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,111 +1,53 @@
 <template>
   <div
-    id="sideBar"
-    class="container-fluid"
+      id="sideBar"
+      class="container-fluid"
   >
     <div class="row">
       <nav
-        id="sidebarMenu"
-        class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+          id="sidebarMenu"
+          class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
       >
         <div class="position-sticky pt-3">
           <ul class="nav flex-column">
             <li class="nav-item">
-              <a
-                class="nav-link active"
-                aria-current="page"
-                href="#"
+              <a @click="changeDashView('Dashboard')"
+                  class="nav-link"
+                  aria-current="page"
+                  href="#"
               >
                 <span data-feather="home"></span>
                 Dashboard
               </a>
             </li>
             <li class="nav-item">
-              <a
-                class="nav-link"
-                href="#"
+              <a @click="changeDashView('Calc')"
+                 class="nav-link"
+                 href="#"
               >
                 <span data-feather="divide-circle"></span>
                 Calculator
               </a>
             </li>
             <li class="nav-item">
-              <a
-                class="nav-link"
-                href="#"
+              <a @click="changeDashView('CurrencyConverter')"
+                 class="nav-link"
+                 href="#"
               >
                 <span data-feather="dollar-sign"></span>
                 Currency Converter
               </a>
             </li>
             <li class="nav-item">
-              <a
-                class="nav-link"
-                href="#"
+              <a @click="changeDashView('Todo')"
+                 class="nav-link"
+                 href="#"
               >
                 <span data-feather="check-circle"></span>
                 TODO
               </a>
             </li>
-            <li class="nav-item">
-              <a
-                class="nav-link"
-                href="#"
-              >
-                <span data-feather="x-circle"></span>
-                Tic Tac Toe
-              </a>
-            </li>
           </ul>
-
-          <!-- <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-            <span>Saved reports</span>
-            <a
-              class="link-secondary"
-              href="#"
-              aria-label="Add a new report"
-            >
-              <span data-feather="plus-circle"></span>
-            </a>
-          </h6>
-          <ul class="nav flex-column mb-2">
-            <li class="nav-item">
-              <a
-                class="nav-link"
-                href="#"
-              >
-                <span data-feather="file-text"></span>
-                Current month
-              </a>
-            </li>
-            <li class="nav-item">
-              <a
-                class="nav-link"
-                href="#"
-              >
-                <span data-feather="file-text"></span>
-                Last quarter
-              </a>
-            </li>
-            <li class="nav-item">
-              <a
-                class="nav-link"
-                href="#"
-              >
-                <span data-feather="file-text"></span>
-                Social engagement
-              </a>
-            </li>
-            <li class="nav-item">
-              <a
-                class="nav-link"
-                href="#"
-              >
-                <span data-feather="file-text"></span>
-                Year-end sale
-              </a>
-            </li>
-          </ul>-->
         </div>
       </nav>
     </div>
@@ -116,12 +58,15 @@
 import * as feather from 'feather-icons'
 
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'SideBar',
+  emits: ['change-dash-view'],
+  methods: {
+    changeDashView: function(viewName){
+      this.$emit("change-dash-view", viewName);
+    }
   },
   mounted() {
-    feather.replace({ 'aria-hidden': 'true' });
+    feather.replace({'aria-hidden': 'true'});
   }
 }
 </script>
